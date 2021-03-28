c640ac9f6cb146fd2890d9424f810ba77fc97ea7
bansheep <j.lynn.weins@gmail.com>
2021-03-27 21:46:42 -0600

    .

--- bookhallow-server/models/Book.js
+++ bookhallow-server/models/Book.js
@@ -7,26 +7,6 @@ const bookSchema = new mongoose.Schema({
     title:{
        type: String,
        required: true
-<<<<<<< HEAD
-    }
-});
-
-// const bookSchema = new mongoose.Schema({
-//     title: String,
-//     authorFirst: String,
-//     authorLast: String,
-//     authorSuffix: String,
-//     additionalAuthors: String,
-//     publisher: String,
-//     originalYear: Number,
-//     publishedYear: Number,
-//     binding: String,
-//     numPages: Number,
-//     isbn: Number,
-//     isbn13: Number,
-//     image:String
-// });
-=======
     },
     authorFirst:{
        type: String,
@@ -51,7 +31,5 @@ const bookSchema = new mongoose.Schema({
 
 });
 
-
->>>>>>> 9a20fbf975ee98d16278bfab43d82b6703bf0bb9
 
 module.exports = Book = mongoose.model('Book', bookSchema);
