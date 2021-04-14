b00049e78e705715ff80bfcead6966865807b493
bansheep <j.lynn.weins@gmail.com>
2021-04-13 21:38:27 -0600

    Setting up front end testing of reach components

---
+++ bookhallow-app/src/setupTests.js
@@ -0,0 +1,5 @@
+// https://medium.com/codeclan/testing-react-with-jest-and-enzyme-20505fec4675
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
